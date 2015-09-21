9c247db2c4288320b13e70e110c49ed873bae658
Andy Bangs <andrew.yuta@gmail.com>
2015-09-21 14:58:18 -0700

    setup layout route

--- routes.js
+++ routes.js
@@ -1,9 +1,20 @@
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        res.render('hello-world');
+        res.render('frame');
     });
 
     app.get('/layout', function(req, res) {
-        //get layout details from parameters
+        var layout;
+
+        // TODO: grab valid layouts from global config file
+        if (req.query.id === '1' || req.query.id === '2') {
+          layout = 'layout-' + req.query.id;
+          res.render(layout);
+        } else {
+          res.status(404);
+          console.error(res.statusCode);
+          res.render('404', { url: req.url });
+          return;
+        }
     });
 };
