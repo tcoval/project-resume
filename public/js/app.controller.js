e5852a05226a7b10dacdfeb11ba76478da7da870
Andy Bangs <andrew.yuta@gmail.com>
2015-09-21 11:08:27 -0700

    added eslint angular style guide

---
+++ public/js/app.controller.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular.module('App', [])
+    .controller('AppController', function () {
+      var vm = this;
+      vm.title = 'Project Resume';
+    })
+})();
