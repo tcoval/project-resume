2b902b7abd4e177474773b71b88af5f091cb41c5
Tanner Coval <tannercoval@gmail.com>
2015-09-21 16:23:15 -0700

    test commit

--- public/js/app.controller.js
+++ public/js/app.controller.js
@@ -13,8 +13,6 @@
             console.log($rootElement);// response.data
           }, function(response) {
             console.log(response);
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
           });
       }
     });
