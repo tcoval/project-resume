89e84ac11c70616fa7b16b0246280078041914ef
Tanner Coval <tannercoval@gmail.com>
2015-09-23 18:32:39 -0700

    added real-time db update

--- public/js/app.controller.js
+++ public/js/app.controller.js
@@ -7,33 +7,16 @@
       var vm = this;
       vm.socket = io.connect('http://localhost:8080');
 
+      vm.authToken = '56032fe92d82e7f12bd78b9f';
+
       vm.emit = function ($event) {
-        var obj;
-
-        // Totally janky and dependant on structure of current template
-        function closestSection(el, className) {
-          while (el.className !== className) {
-            while (el.previousSibling !== null) {
-              el = el.previousSibling;
-              // console.log('previousSibling:', el)
-              if (el.className === className) {
-                // console.log('found!', el);
-                return el.firstChild.data;
-              }
-            }
-
-            el = el.parentNode;
-            // console.log('parentNode:', el)
-          }
-        }
-
-        obj = {
-          section: closestSection($event.target, 'section-title'),
-          attr: $event.target.className,
+        var data = {
+          authToken: vm.authToken,
+          path: $event.target.getAttribute('data'),
           val: $event.target.innerHTML
         };
 
-        vm.socket.emit('value-change', obj);
+        vm.socket.emit('value-change', data);
       }
 
       vm.renderLayout = function () {
--- sockets.js
+++ sockets.js
@@ -1,14 +1,36 @@
+function parsePath(path) {
+  return path.split('.')
+}
+
 module.exports = function(io, Resume, config) {
 
-    io.on('connection', function(socket) {
- 
-      socket.on('value-change', function(data) {
-        var section = data.section;
-        var attr = data.attr;
-        var val = data.val;
+  io.on('connection', function(socket) {
 
-        console.log(data);
+    socket.on('value-change', function(data) {
+      console.log(data);
+      var authToken = data.authToken,
+          path = parsePath(data.path),
+          attr = path.pop(),
+          val = data.val;
+
+      console.log(authToken);
+      console.log(path.join());
+      console.log(attr);
+      console.log(val);
+      Resume.findById(authToken, function(err, resume) {
+        console.log(resume);
+
+        var base;
+        for(var i = 0, base = resume; i < path.length; i++, base = base[path[i]]);
+        console.log(base);
+        base[attr] = val;
+
+        console.log(resume);
+
+        resume.save(function(err) {
+          if(err) console.error(err);
+        });
       });
-
     });
+  });
 };
